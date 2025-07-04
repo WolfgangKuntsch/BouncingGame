7680dd80a9dd0a010ce8e80a247cdf0610f3fee2
Daniel Schätzl <59701285+mintdaniel42@users.noreply.github.com>
2025-07-04 12:09:04 +0200

    Basic bat function

--- Bat.java
+++ Bat.java
@@ -1,12 +1,45 @@
-public class Bat
+public class Bat extends Figur
 {
     public Bat()
     {
-        
+        super();
+        FigurteilFestlegenRechteck(0, 0, 100, 20, "grau");
     }
     
     //Größe des Schlägers setzen
     public void setScale(float scale) { 
-         
+         //EigeneFigurLöschen();
+         //FigurteilFestlegenRechteck(0, 0, , , "grau");
+    }
+    
+    /*
+     * Die  Aktionsmethode für gedrückte Sondertasten.
+     * @param taste KeyCode der gedrückten Taste
+     */
+    @Override void SonderTasteGedrückt (int taste)
+    {
+        switch (taste)
+        {
+            // Pfeil nach links
+          case 37:
+            WinkelSetzen(180);
+            Gehen(10);
+            break;
+            // Pfeil nach oben
+          case 38:
+            WinkelSetzen(90);
+            Gehen(10);
+            break;
+            // Pfeil nach rechts
+          case 39:
+            WinkelSetzen(0);
+            Gehen(10);
+            break;
+            // Pfeil nach unten
+          case 40:
+            WinkelSetzen(270);
+            Gehen(10);
+            break;
+        }
     }
 }
