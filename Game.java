ef63b8275a2c80553d26e535e724694d42753d41
OliverZsuffa <oliver@zsuffa.net>
2025-07-07 14:00:05 +0200

    Added the bricks to the main Game class

--- Game.java
+++ Game.java
@@ -8,6 +8,16 @@
 public class Game extends Ereignisbehandlung
 {
     private Bat character;
+    private BrickController bricks;
+    private Ball ball;
+    
+    private static int BRICK_WIDTH = 80;
+    private static int BRICK_HEIGTH = 20;
+    private static int BRICK_SPACING = 20;
+    
+    private static int NUM_COLUMNS = 6;
+    private static int NUM_ROWS = 8;
+    
     /**
      * Konstruktor für Objekte der Klasse Spiel
      */
@@ -15,17 +25,20 @@ public class Game extends Ereignisbehandlung
     {
         super();
         character = new Bat();
+        ball = new Ball(0, 0, 20, 0, 0);
+        bricks = new BrickController(Zeichenfenster.MalflächenBreiteGeben(), Zeichenfenster.MalflächenHöheGeben(), ball);
         StartGame();
     }
     
     @Override void TaktImpulsAusführen()
     {
-            
+        bricks.frame();       
     }
     
     void StartGame() 
     {
         TaktdauerSetzen(100);
+        bricks.populateGrid(BRICK_WIDTH, BRICK_HEIGTH, NUM_COLUMNS, NUM_ROWS, BRICK_SPACING);
         Starten();    
     }
     
