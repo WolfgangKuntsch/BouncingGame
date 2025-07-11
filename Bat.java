54e4d4471490280b61b7cb8a96d72b0a7e66c27c
Daniel Schätzl <59701285+mintdaniel42@users.noreply.github.com>
2025-07-11 12:14:21 +0200

    Fix controls

--- Bat.java
+++ Bat.java
@@ -30,29 +30,27 @@ public class Bat extends Figur
      * Die  Aktionsmethode für gedrückte Sondertasten.
      * @param taste KeyCode der gedrückten Taste
      */
-    @Override void SonderTasteGedrückt (int taste)
+    @Override void SonderTasteGedrückt(int taste)
     {
         switch (taste)
         {
             // Pfeil nach links + a
             case 37:
-            case 65:
+                if(x <= width/5) break;
                 x -= BAT_STEP;
                 break;
             // Pfeil nach oben
             case 38:
-            case 87:
                 if (!advancedControls) break;
                 y -= BAT_STEP;
                 break;
             // Pfeil nach rechts
             case 39:
-            case 68:
+                if (x >= Zeichenfenster.MalflächenBreiteGeben() - width / 5) break;
                 x += BAT_STEP;
                 break;
             // pfeil nach unten
             case 40:
-            case 83:
                 if (!advancedControls) break;
                 y += BAT_STEP;
                 break;
@@ -61,19 +59,64 @@ public class Bat extends Figur
         PositionSetzen(x, y);
     }
     
+    @Override void TasteGedrückt(char taste) {
+        switch (taste) {
+            case 'w':
+                SonderTasteGedrückt(38);
+                break;
+            case 'a':
+                SonderTasteGedrückt(37);
+                break;
+            case 's':
+                SonderTasteGedrückt(40);
+                break;
+            case 'd':
+                SonderTasteGedrückt(39);
+                break;
+        }
+    }
+    
     public void setAdvancedControls(boolean active) {
         advancedControls = active;
     }
     
     private void draw()
     {
-        FigurteilFestlegenRechteck(width / 2, 0, width, height, "grau");
+        FigurteilFestlegenRechteck(-(width / 2), 0, width, height, "grau");
     }
     
+    private void checkCollisions() {
+        /*
+        switch (CustomMath.CircleRectangleCollision(ball.getXPos(), ball.getYPos(), ball.getRadius(), x, y, height, width)) {
+                case 1: // Top
+                case 2: // Bottom
+                    ball.reflectY();
+                    break;
+            
+                case 3: // Left
+                case 4: // Right
+                    ball.reflectX();
+                    break;
+            
+                case 5: // Top-left
+                case 6: // Top-right
+                case 7: // Bottom-left
+                case 8: // Bottom-right
+                    int dx =  (int) (0.1 * (ball.getXPos() - (brick.getXPos() + brick.getWidth()/2)));
+                    int dy =  (int) (0.1 * (ball.getYPos() - (brick.getYPos() + brick.getHeight()/2)));
+                    ball.setDirection(dx, dy);
+                    
+                    break;
+            }
+        */
+    }
+    
+    @Deprecated(forRemoval=true)
     public int getX() {
         return x;
     }
     
+    @Deprecated(forRemoval=true)
     public int getY() {
         return y;
     }
