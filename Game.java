902b74cf3a62850cc4110df6f77bb6557c1e5c0b
Daniel Schätzl <59701285+mintdaniel42@users.noreply.github.com>
2025-07-16 09:29:26 +0200

    .

--- Game.java
+++ Game.java
@@ -53,7 +53,6 @@ public class Game extends Ereignisbehandlung implements Serializable
         
         background = new Background(screenWidth, screenHeight, BACKGROUND_MIN_STARS, BACKGROUND_MAX_STARS, BACKGROUND_STAR_MIN_RADIUS, BACKGROUND_STAR_MAX_RADIUS, BACKGROUND_STAR_MIN_DECAY, BACKGROUND_STAR_MAX_DECAY, BACKGROUND_STAR_MIN_DELAY, BACKGROUND_STAR_MAX_DELAY);
         bricks = new BrickController(screenWidth, screenHeight, ball, character, new PowerupController(screenWidth, screenHeight, ball, character, POWERUP_RADIUS, POWERUP_SPEED));
-        new Wand ();
         running = false;
         StartGame();
     }
